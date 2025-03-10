7f8d2d0fae25d115e94235ad26c6848158a5689e
arav20603 <dakshandaravind28@gmail.com>
2025-03-10 19:47:24 +0530

    tabs implemented

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function Index() {
+const Index = () => {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-darkMagneta">Welcome</Text>
-      <Link href='/Onboarding'>Onboarding</Link>
-      <Link href='./movie/Avengers'>Avengers</Link>
     </View>
   );
 }
+
+export default Index
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Profiles = () => {
+    return (
+        <View>
+            <Text>Profiles</Text>
+        </View>
+    )
+}
+
+export default Profiles
