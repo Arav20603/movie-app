485a51e795d36b316d3380a34008ec7a6afec34f
arav20603 <dakshandaravind28@gmail.com>
2025-03-11 22:45:30 +0530

    tab icons implemented

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,94 @@
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import {icons} from '@/constants/icons'
+import {images} from '@/constants/images'
+import { ImageBackground, Text, Image, View } from 'react-native'
+
+const TabIcon = ({focused, icon, title} : any) => {
+        if (focused) {
+            return (
+                <ImageBackground
+                source={images.highlight}
+                className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center
+                rounded-full overflow-hidden'
+                >
+                    <Image source={icon}
+                    tintColor='#151315' className='size-5' />
+                    <Text className='text-secondary text-base font-semibold ml-2'>{title}</Text>
+                </ImageBackground>
+            )
+        }
+        return (
+            <View>
+                <Image source={icon} className='size-5 justify-center items-center mt-4 rounded-full' tintColor='#ffff' />
+            </View>
+        )
+        
+}
 
 const _Layout = () => {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: '100%',
+                    height: '100%',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                },
+                tabBarStyle: {
+                    backgroundColor: '#0f0D23',
+                    borderRadius: 50,
+                    marginHorizontal: 20,
+                    marginBottom: 36,
+                    height: 52,
+                    position: 'absolute',
+                    overflow: 'hidden',
+                    borderWidth: 1,
+                    borderColor: '0f0d23'
+                }
+
+            }}
+        >
             <Tabs.Screen 
                 name='index'
                 options={{
                     headerShown: false,
                     title: "Home",
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.home} title='Home' />
+                    )
                 }}
             />
             <Tabs.Screen 
                 name='search'
                 options={{
                     headerShown: false,
-                    title: "Search"
+                    title: "Search",
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.search} title='Search' />
+                    )
                 }}
             />
             <Tabs.Screen 
                 name='profile'
                 options={{
                     headerShown: false,
-                    title: "Profile"
+                    title: "Profile",
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.profile} title='Profile' />
+                    )
                 }}
             />
             <Tabs.Screen 
                 name='saved'
                 options={{
                     headerShown: false,
-                    title: "Saved"
+                    title: "Saved",
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon focused={focused} icon={icons.saved} title='Saved' />
+                    )
                 }}
             />
         </Tabs>
---
+++ constants/images.ts
@@ -0,0 +1,7 @@
+import highlight from '@/assets/images/highlight.png'
+import logo from '@/assets/images/logo.jpg'
+
+export const images = {
+    highlight,
+    logo
+}
